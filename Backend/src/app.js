cdb56379a701b5dd67333020c8abfc7756084ffb
Sneha Poojary <snehapoojary2004@gmail.com>
2025-12-06 11:34:50 +0530

    All setup configured

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,6 +13,13 @@ app.use(cors ({
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
-app.use(express.cookieParser())
+app.use(cookieParser())
+
+// routes
+import userRoutes from './routes/user.routes.js'
+
+
+// routes declaration
+app.use("/api/v1/users",userRoutes)
 
 export {app}
