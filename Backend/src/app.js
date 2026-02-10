0c15c9bdf841540a2819b1fb10d2963e65b6babf
Sneha Poojary <snehapoojary2004@gmail.com>
2026-02-10 19:10:48 +0530

    feat: update CORS configuration to allow multiple origins; enhance security and flexibility for API access

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,8 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin:[" https://nibble-note.vercel.app",
+         "http://localhost:5173"],
   credentials: true
 }));
 
