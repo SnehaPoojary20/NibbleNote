dd3810e1b22ad73f8df810386c55524cd248eba1
Sneha Poojary <snehapoojary2004@gmail.com>
2026-02-10 19:23:47 +0530

    fix: update CORS origin formatting for consistency; improve readability of app.js fix: change axios baseURL to use environment variable; enhance flexibility for API configuration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,11 +6,12 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-  origin:[" https://nibble-note.vercel.app",
-         "http://localhost:5173"],
+  origin: [
+    "https://nibble-note.vercel.app",
+    "http://localhost:5173"
+  ],
   credentials: true
 }));
-
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
