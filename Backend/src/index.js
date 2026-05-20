8d0f577142cc3578c5fb577fe401ce1508a6abf6
Sneha Poojary <snehapoojary2004@gmail.com>
2026-05-20 19:39:02 +0530

    refactor: remove dotenv configuration and clean up index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,20 +1,14 @@
-import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
-
-dotenv.config({
-    path:'./.env'
-})
-
 
 const PORT = process.env.PORT || 2000;
 
 connectDB()
-.then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed", err);
   });
-})
-.catch((err) => {
-  console.log("MongoDB connection failed", err);
-});
