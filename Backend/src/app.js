a741eb9eeaadba1a1d5412b07ca47269adb986eb
Sneha Poojary <snehapoojary2004@gmail.com>
2026-05-28 22:39:31 +0530

    feat: add global error handler middleware and refactor ApiError class

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,14 @@
-import express, { urlencoded } from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
+import { errorHandler } from "./middlewares/error.middleware.js";
 
-const app = express()
+import userRoutes from "./routes/user.routes.js";
+import restaurantRoutes from "./routes/restaurant.routes.js";
+import reviewRoutes from "./routes/review.routes.js";
+
+const app = express();
 
 app.use(cors({
   origin: [
@@ -13,26 +18,52 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true, limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
 
-// routes
+app.use(express.urlencoded({
+  extended: true,
+  limit: "16kb"
+}));
+
+app.use(express.static("public"));
+
+app.use(cookieParser());
+
+
+// HOME ROUTE
 app.get("/", (req, res) => {
   res.send("NibbleNote API Running 🚀");
 });
-import userRoutes from './routes/user.routes.js'
-import restaurantRoutes from './routes/restaurant.routes.js'
-import reviewRoutes from './routes/review.routes.js'
 
 
-// routes declaration
-app.use("/api/v1/users", (req, res, next) => {
-  console.log("Users route hit:", req.method, req.url);
-  next();
-}, userRoutes);
-app.use("/api/v1/restaurants",restaurantRoutes)
-app.use("/api/v1/reviews",reviewRoutes)
+// ROUTES
+app.use(
+  "/api/v1/users",
+  (req, res, next) => {
+    console.log(
+      "Users route hit:",
+      req.method,
+      req.url
+    );
 
-export {app}
+    next();
+  },
+  userRoutes
+);
+
+app.use(
+  "/api/v1/restaurants",
+  restaurantRoutes
+);
+
+app.use(
+  "/api/v1/reviews",
+  reviewRoutes
+);
+
+
+// GLOBAL ERROR HANDLER
+// MUST BE LAST
+app.use(errorHandler);
+
+export { app };
