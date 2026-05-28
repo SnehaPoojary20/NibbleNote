f930fc31b9deb9ff12ecfd90700571cd35165515
Sneha Poojary <snehapoojary2004@gmail.com>
2026-05-28 19:22:28 +0530

    fix: add logging for request cookies and authorization header in JWT verification

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -5,6 +5,10 @@ import { User } from "../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
+
+    console.log("REQ COOKIES:", req.cookies);
+    console.log("AUTH HEADER:", req.header("Authorization"));
+
     const token =
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
